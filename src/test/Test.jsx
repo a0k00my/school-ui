42bbd8666417c9a2fc4a4ad93be5e158c5b4d341
a0k00my <adityanitjsr@gmail.com>
2018-09-30 21:58:36 +0530

    #6

--- src/test/Test.jsx
+++ src/test/Test.jsx
@@ -1,8 +1,62 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
 class Test extends Component {
   render() {
-    return <div>Welcome to Avanti Bai Girls Inter College..</div>;
+    return (
+      <Grid>
+        <Paper>
+          <br />
+          <Typography variant="display1" align="center">
+            Welcome to Avanti bai school
+          </Typography>
+          <br />
+        </Paper>
+        <br />
+        <Paper>
+          <br />
+          <Typography variant="display1" align="center">
+            Our Future
+          </Typography>
+          <br />
+        </Paper>
+        <br />
+        <Paper>
+          <br />
+          <Typography variant="display1" align="center">
+            Our Mission
+          </Typography>
+          <br />
+        </Paper>
+        <br />
+        <Paper>
+          <br />
+          <Typography variant="display1" align="center">
+            Sucess Story
+          </Typography>
+          <br />
+        </Paper>
+        <br />
+        <Paper>
+          <br />
+          <Typography variant="display1" align="center">
+            Gallery
+          </Typography>
+          <br />
+        </Paper>
+        <br />
+        <Paper>
+          <br />
+          <Typography variant="display1" align="center">
+            Our Contact
+          </Typography>
+          <br />
+        </Paper>
+      </Grid>
+    );
   }
 }
 
